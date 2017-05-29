a163dabd3b571c6f998eb2aac88f532e60723a12
Neto Gameleira <jgdrneto@gmail.com>
2017-05-29 14:28:19 -0300

    Adequando o código a beagleboneblack defeituosa

--- Joystick.cpp
+++ Joystick.cpp
@@ -19,7 +19,7 @@ class Joystick{
             
     	    GPIO::setup(PORTNUMBER::P9_39);			        //Porta analógica do potenciômetro
     	    GPIO::setup(PORTNUMBER::P9_14,DIRECTION::IN); 	//Porta digital do botão 1 (ACTION)
-            GPIO::setup(PORTNUMBER::P9_15,DIRECTION::IN);   //Porta digital do botão 2 (UP)
+            GPIO::setup(PORTNUMBER::P9_41,DIRECTION::IN);   //Porta digital do botão 2 (UP)
             GPIO::setup(PORTNUMBER::P9_16,DIRECTION::IN);   //Porta digital do botão 3 (DOWN)
         }
         
@@ -28,11 +28,11 @@ class Joystick{
         }
         
         int valorBotaoUp(){
-            return GPIO::input(PORTNUMBER::P9_14);
+            return GPIO::input(PORTNUMBER::P9_41);
         }
 
         int valorBotaoDown(){
-            return GPIO::input(PORTNUMBER::P9_15);
+            return GPIO::input(PORTNUMBER::P9_16);
         }
    
         int escolherVelocidade(){
